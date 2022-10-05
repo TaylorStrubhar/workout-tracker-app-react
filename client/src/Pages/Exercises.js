1b33c737d7e890851317e8e2ac4fa23b55ae0b43
J0J0C0DING <102717784+J0J0C0DING@users.noreply.github.com>
2022-10-04 21:59:06 -0500

    Removed unused imports

--- client/src/Pages/Exercises.js
+++ client/src/Pages/Exercises.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-import { TextField } from '@mui/material';
+
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
-
-import IconButton from '@mui/material/IconButton';
-import DeleteIcon from '@mui/icons-material/Delete';
 
 import { EditExerciseModal, AddExerciseModal } from '../components/exerciseModals';
 
