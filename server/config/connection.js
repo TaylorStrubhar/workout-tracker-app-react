4bb12fc74d7d46e772a60eeeec0702e75514b5ff
Ira Strubhar <taylor.strubhar@gmail.com>
2022-12-31 13:04:17 -0800

    adjusted mongoose connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://IraFranklin:JockoWork9900!!@cluster0.fr0nbst.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
